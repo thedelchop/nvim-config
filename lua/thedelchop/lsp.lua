0f2991ac3d27501b623128e48e0b9a03ae4efd7a
Joseph DelCioppio <jdelcioppio@mojotech.com>
2021-07-26 15:57:24 -0400

    Remove async formatting function, its outdated

--- lua/thedelchop/lsp.lua
+++ lua/thedelchop/lsp.lua
@@ -4,20 +4,6 @@ local augroup = require("thedelchop.utils").augroup
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-vim.lsp.handlers["textDocument/formatting"] = function(err, _, result, _, bufnr)
-    if err ~= nil or result == nil then
-        return
-    end
-    if not vim.api.nvim_buf_get_option(bufnr, "modified") then
-        local view = vim.fn.winsaveview()
-        vim.lsp.util.apply_text_edits(result, bufnr)
-        vim.fn.winrestview(view)
-        if bufnr == vim.api.nvim_get_current_buf() then
-            vim.api.nvim_command("noautocmd :update")
-        end
-    end
-end
-
 local on_attach = function(client, _)
 
     vim.cmd [[imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']]
